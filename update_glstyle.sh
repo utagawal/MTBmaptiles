b214ac80c402fc35166f6954dab176b55fc6c0c2
olivier <olivier@omrs.fr>
2022-07-12 15:20:38 +0200

    first version of automatic style update

--- update_glstyle.sh
+++ update_glstyle.sh
@@ -2,26 +2,85 @@
 CLEARDATE=$(date "+%d%m%y_%Hh%M")
 TMPDIR="/tmp/glstyle/"
 
+LOGDIR="/var/data/logs/"
+LOGFILE="update_glstyle.log"
+
 GLSTYLENAME="utagawavtt"
-GLSTYLEDIR="/var/data/style/"
+GLSTYLEDIR="/var/data/styles/"
 
-DOCKERRELOADCMD=$(docker exec tileserver-gl bash -c 'kill -HUP $(ls -l /proc/*/exe | sed -n "/\/node$/s/.*proc\/\([0-9]\+\)\/exe .*/\1/p")')
+CTNAME="tileserver-gl"
 
-mkdir -p ${TMPDIR}
+ctReload() {
+	docker exec ${1} bash -c 'kill -HUP $(ls -l /proc/*/exe | sed -n "/\/node$/s/.*proc\/\([0-9]\+\)\/exe .*/\1/p")'
+}
+
+ctStart() {
+	docker run --name ${1} -d --rm -it -v /var/data:/data -p 8080:80 maptiler/tileserver-gl
+}
+
+ctStatus() {
+	docker ps -f NAME=${1} -f STATUS=running -q
+}
+
+writeToLog() {
+	local LOGTIME=$(date "+%d-%m-%y %H:%M:%S")
+	echo ${LOGTIME} ${1} >> ${LOGDIR}${LOGFILE}
+}
+
+mkdir -p ${TMPDIR} ${LOGDIR}
 cd ${TMPDIR}
 
 # Downloading style file on the repository
-wget -q https://github.com/utagawal/MTBmaptiles/blob/main/utagawavtt.json
+writeToLog "Downloading style file"
+wget -q https://raw.githubusercontent.com/utagawal/MTBmaptiles/main/utagawavtt.json
 
 if [ $? -eq 0 ]; then
+	writeToLog "Style file downloaded"
 	# Comparing repository's style file and local style file
+	cmp -s ${GLSTYLEDIR}${GLSTYLENAME}/style.json ${TMPDIR}/utagawavtt.json
+
+	if [ $? -eq 0 ]; then
+		writeToLog "Style has not been updated"
+		rm ${TMPDIR}/utagawavtt.json
+	else
 	# If files are differents..
-	if cmp -s ${GLSTYLEDIR}${GLSTYLENAME}/style.json ${TMPDIR}/utagawavtt.json; then
+		writeToLog "New style been processing.."
 		# we save the local file..
+		writeToLog "Saving old style to ${GLSTYLEDIR}${GLSTYLENAME}/style.json_${CLEARDATE}"
 		cp ${GLSTYLEDIR}${GLSTYLENAME}/style.json ${GLSTYLEDIR}${GLSTYLENAME}/style.json_${CLEARDATE}
 		# and replace it with the distant file..
-		mv ${TMPDIR}/utagawavtt.json ${GLSTYLEDIR}${GLSTYLENAME}/style.json
-		# then we reload tileserver-gl
-		$DOCKERRELOADCMD
+		writeToLog "Applying new style"
+		cp ${TMPDIR}/utagawavtt.json ${GLSTYLEDIR}${GLSTYLENAME}/style.json
+		rm ${TMPDIR}/utagawavtt.json
+
+		if [ -z $(ctStatus $CTNAME) ]; then
+			# Container not running, so we start it
+			writeToLog "Starting tileserver-gl"
+			ctStart $CTNAME
+
+			sleep 10
+
+			if [ -z $(ctStatus $CTNAME) ]; then
+				writeToLog "There is a problem while launching tileserver-gl container"
+				echo "There is a problem while launching tileserver-gl container" | mail -s "tilserver-gl problem" ${ADMINEMAIL}
+				exit
+			else
+				writeToLog "tileserver-gl started successfully"
+			fi
+		else
+			# Container is running, we ask it to reload tileserver-gl config file
+			writeToLog "Reloading tileserver-gl"
+			ctReload $CTNAME
+
+			if [ -z $(ctStatus $CTNAME) ]; then
+				writeToLog "There is a problem while reloading tileserver-gl config"
+				echo "There is a problem while reloading tileserver-gl config" | mail -s "tilserver-gl problem" ${ADMINEMAIL}
+				exit
+			else
+				writeToLog "tileserver-gl reloaded successfully"
+			fi
+		fi
 	fi
+else
+	writeToLog "Problem while downloading style from the repository : ${?}"
 fi
